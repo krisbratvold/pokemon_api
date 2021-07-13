bf00fdbc8b9b67dc9504bdb19f06bf2775ccfdb7
krisbratvold <kris.bratvold@gmail.com>
2021-07-13 13:24:37 -0700

    changed to axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
 
-    const [pokemon,setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
 
-    const ShowPoke = () => {
-      fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-      .then(response => response.json())
-      .then(response => setPokemon(response.results))
-    };
+  const ShowPoke = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {
+        console.log(response);
+        setPokemon(response.data.results)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={ShowPoke}>Fetch Pokemon</button>
-        <div>
-          {pokemon.map((pokemon,i) => {
-            return ( <ul key={i}><li>{pokemon.name}</li></ul> )
-          })}
-        </div>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={ShowPoke}>Fetch Pokemon</button>
+          <div>
+            {pokemon.map((pokemon, i) => {
+              return (<ul key={i}><li>{pokemon.name}</li></ul>)
+            })}
+          </div>
+        </header>
+      </div>
+    );
+  }
+
 
 export default App;
